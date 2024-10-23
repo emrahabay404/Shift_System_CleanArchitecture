219459f70bb13b2ba89967003dfc719cff5a8aff
Emrah_Abay <Emrah_Abay>
2024-10-23 23:04:06 +0300

    dinamik_sorgu_ve_sayfalama_1

---
+++ Shift_System.Application/Features/Shifts/Queries/GetShiftsDinamik.cs
@@ -0,0 +1,105 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shift_System.Application.Extensions;
+using Shift_System.Application.Interfaces.Repositories;
+using Shift_System.Domain.Entities.Tables;
+using Shift_System.Shared.Helpers;
+
+namespace Shift_System.Application.Features.Shifts.Queries
+{
+    public record GetShiftsDinamik : IRequest<PaginatedResult<GetAllShiftsDto>>
+    {
+        public DynamicQuery Query { get; }
+
+        public GetShiftsDinamik(DynamicQuery query)
+        {
+            Query = query;
+        }
+    }
+
+    internal class GetShiftsDinamikHandler : IRequestHandler<GetShiftsDinamik, PaginatedResult<GetAllShiftsDto>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetShiftsDinamikHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<PaginatedResult<GetAllShiftsDto>> Handle(GetShiftsDinamik request, CancellationToken cancellationToken)
+        {
+            var shiftsQuery = _unitOfWork.Repository<ShiftList>().Entities.AsQueryable();
+
+            // Filtreleme uygulama
+            if (request.Query.Filter != null)
+            {
+                shiftsQuery = ApplyFilter(shiftsQuery, request.Query.Filter);
+            }
+
+            // Sıralama uygulama
+            if (request.Query.Sort != null && request.Query.Sort.Any())
+            {
+                foreach (var sort in request.Query.Sort)
+                {
+                    if (sort.Dir.ToLower() == "desc")
+                    {
+                        shiftsQuery = shiftsQuery.OrderByDescending(e => EF.Property<object>(e, sort.Field));
+                    }
+                    else
+                    {
+                        shiftsQuery = shiftsQuery.OrderBy(e => EF.Property<object>(e, sort.Field));
+                    }
+                }
+            }
+
+            var paginatedList = await shiftsQuery
+                .ProjectTo<GetAllShiftsDto>(_mapper.ConfigurationProvider)
+                .ToPaginatedListAsync(request.Query.Page, request.Query.PageSize, cancellationToken);
+
+            return PaginatedResult<GetAllShiftsDto>.SuccessAsync(
+                paginatedList.Data,
+                paginatedList.TotalCount,
+                paginatedList.CurrentPage,
+                paginatedList.PageSize,
+                Messages.Teams_Listed_TR
+            );
+        }
+
+        private IQueryable<ShiftList> ApplyFilter(IQueryable<ShiftList> query, Filter filter)
+        {
+            // IsDeleted = false filtresi her zaman uygulanır
+            query = query.Where(e => e.IsDeleted == false);
+
+            // Ana filtreyi uygulama
+            query = ProcessFilter(query, filter);
+            return query;
+        }
+
+        private IQueryable<ShiftList> ProcessFilter(IQueryable<ShiftList> query, Filter filter)
+        {
+            if (filter.Logic == "and")
+            {
+                query = query.Where(e => EF.Property<object>(e, filter.Field).ToString() == filter.Value.ToString());
+            }
+            else if (filter.Logic == "or")
+            {
+                // "or" mantığını ekleyebilirsiniz
+            }
+
+            // Alt filtreleri kontrol et
+            if (filter.Filters != null && filter.Filters.Any())
+            {
+                foreach (var subFilter in filter.Filters)
+                {
+                    query = ProcessFilter(query, subFilter);
+                }
+            }
+
+            return query;
+        }
+    }
+}
--- Shift_System.WebAPI/Controllers/AdminController.cs
+++ Shift_System.WebAPI/Controllers/AdminController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Shift_System.Application.Features.Shifts.Queries;
+using Shift_System.Shared.Helpers;
 
 namespace Shift_System.WebAPI.Controllers
 {
@@ -8,6 +11,23 @@ namespace Shift_System.WebAPI.Controllers
     [Authorize(Roles = "Manager,Admin")]
     public class AdminController : ApiControllerBase
     {
+        private readonly IMediator _mediator;
+        public AdminController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+
+        [HttpPost("GetShiftsWithPagination")]
+        [AllowAnonymous]
+        public async Task<ActionResult<PaginatedResult<GetAllShiftsDto>>> GetShiftsWithPagination([FromBody] DynamicQuery query)
+        {
+            var request = new GetShiftsDinamik(query);
+            var resultData = await _mediator.Send(request);
+            return Ok(resultData);
+        }
+
 
     }
 }
